2ddf46d1962334de0e25c75d4dcda80d12a34e4e
Arya1119 <20it082@charusat.edu.in>
2023-01-18 01:04:06 +0530

    Delegated Transfer

--- contracts/AryaToken.sol
+++ contracts/AryaToken.sol
@@ -15,8 +15,16 @@ contract AryaToken {
     address indexed _to,
     uint256 _value
     );
-
+    
+  //approve
+  event Approval(
+    address indexed _owner,
+    address indexed _spender,
+    uint256 _value
+  );  
   mapping(address => uint256) public balanceOf;
+  mapping(address=>mapping(address => uint256)) public allowance;
+  //allowance
 
   constructor(uint256 _initialSupply){
     balanceOf[msg.sender]=_initialSupply;
@@ -35,4 +43,36 @@ contract AryaToken {
  emit Transfer(msg.sender, _to, _value);
   return true;
   }
+
+  //approve function
+ function approve(address _spender,uint256 _value) public returns(bool success){
+  //allowance
+  allowance[msg.sender][_spender]=_value;
+  //approve events
+  emit Approval(msg.sender,_spender,_value);
+  return true;
+ }
+  //transferFrom
+ function transferFrom(address _from, address _to, uint256 _value) public returns(bool success){
+  //require from has enough tokens
+  require(_value<=balanceOf[_from]);
+  
+  //require allowance is big enough 
+  require(_value<=allowance[_from][msg.sender]);
+  
+  //change the balance
+  balanceOf[_from] -= _value;
+  balanceOf[_to] += _value;
+  
+  //update the allowance
+  allowance[_from][msg.sender]-=_value;
+  
+  //transfer event
+  emit Transfer(_from, _to, _value);
+  
+  //return boolean
+  return true;
+ }
 }
+
+
