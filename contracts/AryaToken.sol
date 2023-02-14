f75209491cdafaee887230ce2bc49eb8e0ee20d0
Arya1119 <20it082@charusat.edu.in>
2023-02-14 14:15:15 +0530

    Token Sale

--- contracts/AryaToken.sol
+++ contracts/AryaToken.sol
@@ -1,78 +1,66 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.17;
 
 contract AryaToken {
-  //name
-  string public name="Arya Token";
-  //symbol
-  string public symbol="Arya";
-  //standard
-  string public standard="Arya Token v1.0";
-  uint256 public totalSupply;
-
-  event Transfer(
-    address indexed _from,
-    address indexed _to,
-    uint256 _value
-    );
     
-  //approve
-  event Approval(
-    address indexed _owner,
-    address indexed _spender,
-    uint256 _value
-  );  
-  mapping(address => uint256) public balanceOf;
-  mapping(address=>mapping(address => uint256)) public allowance;
-  //allowance
+    //name
+    string public name = 'Arya Token';
+    //symbol
+    string public symbol = 'Arya';
+    string public standard = 'Arya Token v1.0';
+    uint256 public totalSupply;
 
-  constructor(uint256 _initialSupply){
-    balanceOf[msg.sender]=_initialSupply;
-    totalSupply=_initialSupply;
-    //allocate the initial supply
-  }
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
 
-  //transfer
-  function transfer(address _to,uint256 _value) public returns (bool success){
-  //exception if account doesn't enough
-  require(balanceOf[msg.sender] >= _value);
-  //transfer the balance
-  balanceOf[msg.sender] -= _value;
-  balanceOf[_to] += _value;
+     event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
- emit Transfer(msg.sender, _to, _value);
-  return true;
-  }
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
-  //approve function
- function approve(address _spender,uint256 _value) public returns(bool success){
-  //allowance
-  allowance[msg.sender][_spender]=_value;
-  //approve events
-  emit Approval(msg.sender,_spender,_value);
-  return true;
- }
-  //transferFrom
- function transferFrom(address _from, address _to, uint256 _value) public returns(bool success){
-  //require from has enough tokens
-  require(_value<=balanceOf[_from]);
-  
-  //require allowance is big enough 
-  require(_value<=allowance[_from][msg.sender]);
-  
-  //change the balance
-  balanceOf[_from] -= _value;
-  balanceOf[_to] += _value;
-  
-  //update the allowance
-  allowance[_from][msg.sender]-=_value;
-  
-  //transfer event
-  emit Transfer(_from, _to, _value);
-  
-  //return boolean
-  return true;
- }
+    constructor (uint256 _initialSupply) {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+        //allocate the initial supply
+    }
+
+    //transfer
+    function transfer(address _to, uint256 _value) public returns(bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
 }
-
-
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
 var AryaToken = artifacts.require("./AryaToken.sol");
+var AryaTokenSale = artifacts.require("./AryaTokenSale.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(AryaToken,1000000);
-}; 
+  deployer.deploy(AryaToken,1000000).then(function(){
+    //token price is 0.001 ether
+    var tokenPrice = 1000000000000000; 
+    return deployer.deploy(AryaTokenSale,AryaToken.address,tokenPrice);
+  });
+  
+};
